95d4607f16bfc254d62ba7b49c9aa99b84bcf658
edom18 <edo.m18@gmail.com>
2017-10-12 08:49:46 +0900

    ざっくりコード追記

---
+++ Assets/CurlNoise/Scripts/CurlNoise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CurlNoiseSample
+{
+    public class CurlNoise : MonoBehaviour
+    {
+        [SerializeField]
+        private ComputeShader _shader;
+
+        private void Start()
+        {
+            int kernelIndex = _shader.FindKernel("CurlNoiseMain");
+
+            int num = 10;
+            ComputeBuffer buffer = new ComputeBuffer(num, sizeof(int));
+
+            _shader.SetBuffer(kernelIndex, "Result", buffer);
+            _shader.Dispatch(kernelIndex, num, 1, 1);
+
+            int[] data = new int[num];
+            buffer.GetData(data);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Debug.Log(data[i]);
+            }
+
+            buffer.Release();
+        }
+    }
+}
