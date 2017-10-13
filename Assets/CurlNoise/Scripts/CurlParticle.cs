fa8ae9f62ac8798a854fe26245ae0e020e6e92c9
edom18 <edo.m18@gmail.com>
2017-10-14 01:53:13 +0900

    マージしたメッシュをレンダリングするところまで実装

---
+++ Assets/CurlNoise/Scripts/CurlParticle.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CurlNoiseSample
+{
+    public struct Particle
+    {
+        public int id;
+        public Vector3 position;
+        public float time;
+        public float liefTime;
+    }
+
+    public class CurlParticle : MonoBehaviour
+    {
+        // 1メッシュに入る最大長点数
+        const int MAX_VERTEX_NUM = 65534;
+
+        [SerializeField]
+        private int _maxParticleNum = 1000;
+
+        [SerializeField]
+        private Mesh _mesh;
+
+        [SerializeField]
+        private Shader _shader;
+
+        [SerializeField]
+        ComputeShader _computeShader;
+
+        private Mesh _combinedMesh;
+        private List<Material> _materials = new List<Material>();
+
+        private ComputeBuffer _particles;
+
+        private int _kernelIndex;
+        private int _particleNumPerMesh;
+        private int _meshNum;
+
+        private void OnDisable()
+        {
+            _particles.Release();
+        }
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Update()
+        {
+            UpdatePosition();
+        }
+
+        /// <summary>
+        /// 指定した数分、マージしたメッシュを生成する
+        /// </summary>
+        /// <param name="mesh">元となるメッシュ</param>
+        /// <param name="num">生成するメッシュ数</param>
+        /// <returns>マージされたメッシュ</returns>
+        private Mesh CreateCombinedMesh(Mesh mesh, int num)
+        {
+            Assert.IsTrue(mesh.vertexCount * num <= MAX_VERTEX_NUM);
+
+            int[] meshIndices = mesh.GetIndices(0);
+            int indexNum = meshIndices.Length;
+
+            // Buffer
+            int[] indices = new int[num * indexNum];
+            List<Vector2> uv0 = new List<Vector2>();
+            List<Vector2> uv1 = new List<Vector2>();
+            List<Vector3> vertices = new List<Vector3>();
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector4> tangents = new List<Vector4>();
+
+            for (int id = 0; id < num; id++)
+            {
+                vertices.AddRange(mesh.vertices);
+                normals.AddRange(mesh.normals);
+                tangents.AddRange(mesh.tangents);
+                uv0.AddRange(mesh.uv);
+
+                // 各メッシュのIndexは、1つのモデルの頂点数 * ID分ずらす
+                for (int n = 0; n < indexNum; n++)
+                {
+                    indices[id * indexNum + n] = id * mesh.vertexCount + meshIndices[n];
+                }
+
+                // 2番目のUVにIDを格納しておく
+                for (int n = 0; n < mesh.uv.Length; n++)
+                {
+                    uv1.Add(new Vector2(id, id));
+                }
+            }
+
+            Mesh combinedMesh = new Mesh();
+            combinedMesh.SetVertices(vertices);
+            combinedMesh.SetIndices(indices, MeshTopology.Triangles, 0);
+            combinedMesh.SetNormals(normals);
+            combinedMesh.RecalculateNormals();
+            combinedMesh.SetTangents(tangents);
+            combinedMesh.SetUVs(0, uv0);
+            combinedMesh.SetUVs(1, uv1);
+            combinedMesh.RecalculateBounds();
+            combinedMesh.bounds.SetMinMax(Vector3.one * -100f, Vector3.one * 100f);
+
+            return combinedMesh;
+
+        }
+
+        private void UpdatePosition()
+        {
+            _computeShader.SetBuffer(_kernelIndex, "_Particles", _particles);
+            _computeShader.SetFloat("_DeltaTime", Time.deltaTime);
+            _computeShader.Dispatch(_kernelIndex, _maxParticleNum / 8, 1, 1);
+
+            for (int i = 0; i < _meshNum; i++)
+            {
+                Material material = _materials[i];
+                material.SetInt("_IdOffset", _particleNumPerMesh * i);
+                material.SetBuffer("_Particles", _particles);
+                Graphics.DrawMesh(_combinedMesh, transform.position, transform.rotation, material, 0);
+            }
+        }
+
+        private Particle[] GenerateParticles()
+        {
+            Particle[] particles = new Particle[_maxParticleNum];
+
+            for (int i = 0; i < _maxParticleNum; i++)
+            {
+                float x = Random.Range(-50f, 50f);
+                float y = Random.Range(-50f, 50f);
+                float z = Random.Range(-50f, 50f);
+                Particle p = new Particle
+                {
+                    id = i,
+                    position = new Vector3(x, y, z),
+                    time = 0,
+                    liefTime = 10f,
+                };
+
+                particles[i] = p;
+            }
+
+            return particles;
+        }
+
+        private void Initialize()
+        {
+            _particleNumPerMesh = MAX_VERTEX_NUM / _mesh.vertexCount;
+            _meshNum = (int)Mathf.Ceil((float)_maxParticleNum / _particleNumPerMesh);
+
+            for (int i = 0; i < _meshNum; i++)
+            {
+                Material material = new Material(_shader);
+                material.SetInt("_IdOffset", _particleNumPerMesh * i);
+                _materials.Add(material);
+            }
+
+            _combinedMesh = CreateCombinedMesh(_mesh, _particleNumPerMesh);
+
+            _particles = new ComputeBuffer(_maxParticleNum, Marshal.SizeOf(typeof(Particle)));
+
+            Particle[] particles = GenerateParticles();
+            _particles.SetData(particles);
+
+            _kernelIndex = _computeShader.FindKernel("CurlNoiseMain");
+
+            // ランダムな値を初期値として与える
+            _computeShader.SetFloat("randomX1", Random.value);
+            _computeShader.SetFloat("randomY1", Random.value);
+            _computeShader.SetFloat("randomZ1", Random.value);
+            _computeShader.SetFloat("randomX2", Random.value);
+            _computeShader.SetFloat("randomY2", Random.value);
+            _computeShader.SetFloat("randomZ2", Random.value);
+        }
+    }
+}
---
+++ Assets/CurlNoise/Shaders/ParticleShader.shader
@@ -0,0 +1,84 @@
+﻿Shader "Custom/ParticleShader"
+{
+    Properties
+    {
+        _Color("Color", Color) = (1,1,1,1)
+    }
+
+    SubShader
+    {
+        Tags { "RenderType" = "Transparent" "Queue" = "Transparent" }
+        LOD 200
+
+        CGINCLUDE
+        #include "UnityCG.cginc"
+        #include "UnityStandardShadow.cginc"
+
+        struct Particle
+        {
+            int id;
+            float3 position;
+            float time;
+            float lifeTime;
+        };
+
+#ifdef SHADER_API_D3D11
+        StructuredBuffer<Particle> _Particles;
+#endif
+
+        int _IdOffset;
+
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float3 normal : NORMAL;
+            float2 uv1 : TEXCOORD1;
+        };
+
+        struct v2f
+        {
+            float4 position : SV_POSITION;
+            float3 normal : NORMAL;
+            float2 uv1 : TEXCOORD1;
+        };
+
+        inline int getId(float2 uv1)
+        {
+            return (int)(uv1.x + 0.5) + _IdOffset;
+        }
+
+        v2f vert(appdata v)
+        {
+#ifdef SHADER_API_D3D11
+            Particle p = _Particles[getId(v.uv1)];
+            v.vertex.xyz += p.position;
+#endif
+
+            v2f o;
+            o.uv1 = v.uv1;
+            o.position = UnityObjectToClipPos(v.vertex);
+            o.normal = v.normal;
+            return o;
+        }
+
+        float4 frag(v2f i) : SV_Target
+        {
+            return _Color;
+        }
+        ENDCG
+
+        Pass
+        {
+            ZWrite On
+            ZTest LEqual
+            Cull Off
+
+            CGPROGRAM
+            #pragma target 3.0
+            #pragma vertex vert
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+    FallBack "Diffuse"
+}
